bdcdc9b93f6dba3b88290134e7f1b51c2189410b
Gustavo Suto <guyrux@gmail.com>
2022-07-17 15:38:29 -0300

    :ambulance: Added a try-except to avoid an FileError Eception.

--- src/custom_logger.py
+++ src/custom_logger.py
@@ -14,17 +14,21 @@ def get_logger(log_file: str = './log/file.log') -> logging.getLogger:
     logger.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(log_file)
-
     console_handler.setLevel(logging.DEBUG)
-    file_handler.setLevel(logging.ERROR)
-
     console_handler.setFormatter(logging.Formatter('%(levelname)-8s | %(filename)s:%(lineno)-4s | %(message)s'))
-    file_handler.setFormatter(logging.Formatter('%(asctime)s; %(levelname)-s; %(filename)-s:%(lineno)-s; %(message)s', '%Y-%m-%d %H:%M'))
+    
+    try:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.ERROR)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s; %(levelname)-s; %(filename)-s:%(lineno)-s; %(message)s', '%Y-%m-%d %H:%M'))
 
-    if not logger.hasHandlers():
-        logger.addHandler(console_handler)
-        logger.addHandler(file_handler)
+        if not logger.hasHandlers():
+            logger.addHandler(console_handler)
+            logger.addHandler(file_handler)
+
+    except:
+        if not logger.hasHandlers():
+            logger.addHandler(console_handler)
 
     return logger
 
