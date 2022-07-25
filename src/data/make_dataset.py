1e950bd471cc1a3fe2d2535d8fc15a2bd861bc05
Gustavo Suto <guyrux@gmail.com>
2022-07-24 21:57:41 -0300

    :rotating_light: Fixed linter warnings.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -9,6 +9,7 @@ from dynaconf import Dynaconf
 
 config = Dynaconf(settings_files=["settings.toml"])
 
+
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -19,7 +20,7 @@ def main(input_filepath, output_filepath):
 
 if __name__ == '__main__':
     path = Path().absolute()
-    
+
     if path.name == 'data':
         os.chdir(Path(__file__).resolve().parents[2])
         from src.custom_logger import get_logger
